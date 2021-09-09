554ec4a612eca2ce4ca6c69fdf2399571cbf7ee5
Hristo Gatsinski <gatsinski@gmail.com>
2021-09-09 19:32:13 +0300

    Add Users API

--- conquiztador/urls.py
+++ conquiztador/urls.py
@@ -19,7 +19,11 @@ from django.urls import include, path
 from rest_framework.authtoken import views as authtoken_views
 from rest_framework.routers import DefaultRouter
 
+from conquiztador.contrib.users.views import UserViewSet
+
 router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
